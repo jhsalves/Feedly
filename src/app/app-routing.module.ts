35b4d8bda3b4b3954c8224413c276e3e260615d3
Jonathas <jhsalves@gmail.com>
2018-11-21 23:22:39 -0200

    Login page ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'Signup', loadChildren: './signup/signup.module#SignupPageModule' },
 ];
 
 @NgModule({
